dd220c013054b96a1751919d753d54c471d0384d
lokeshshirode45 <lokeshshirode88876@gmail.com>
2024-08-13 23:52:20 +0530

    Project Setup and Done Routing

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h2>Hey Its me Lokesh!!</h2>
+      <Button>Click Me!</Button>
+    </>
+  )
+}
+
+export default App
