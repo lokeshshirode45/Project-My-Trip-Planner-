e2dd275a5b210978ae45c25c7a4d72b2203d76fa
lokeshshirode45 <lokeshshirode88876@gmail.com>
2024-08-14 22:09:48 +0530

    Place api and form development

--- src/create-trip/index.jsx
+++ src/create-trip/index.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React, {useState} from 'react'
+import GooglePlacesAutocomplete from "react-google-places-autocomplete";
+import { Input } from '@/components/ui/input';
 
 function CreateTrip() {
+  const [place, setplace]=useState() ; 
   return (
-    <div>CreateTrip</div>
-  )
+    <div className="sm:px-10 md:px-32 lg:px-56 xl:px-10 px-5 mt-10">
+      <h2 className="font-bold text-3xl">Tell us your travel preference</h2>
+      <p className="mt-3 text-gray-500 text-xl">
+        Just provide some basic information, and our trip planner will generate
+        a customized itinerary based on your preference
+      </p>
+
+      <div className="mt-20">
+        <div>
+          <h2 className="text-xl my-3 font-medium">
+            What is destination of choice?
+          </h2>
+          <GooglePlacesAutocomplete
+            apiKey={import.meta.env.VITE_GOOGLE_PLACE_API_KEY}
+            selectProps={{
+              place,
+              onChange: (v) => {setplace(v);console.log(v);}
+            }}
+          />
+        </div>
+        <div>
+        <h2 className="text-xl my-3 font-medium">
+          How many days are you planning your trip?</h2>
+        <Input placeholder={'Ex.3'} type="number" />  
+        </div>    
+            
+      </div>
+            
+    </div>
+  );
 }
 
-export default CreateTrip
+export default CreateTrip;
